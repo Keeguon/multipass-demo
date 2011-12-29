3137856bd6ce6b093ab43c529f02ab6d107e1b2f
Félix Bellanger <felix.bellanger@gmail.com>
2011-12-29 15:09:04 +0100

    Initial commit

---
+++ bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+
+if (false === class_exists('Symfony\Component\ClassLoader\UniversalClassLoader', false)) {
+  require_once __DIR__.'/vendor/Symfony/Component/ClassLoader/UniversalClassLoader.php';
+}
+
+use Symfony\Component\ClassLoader\UniversalClassLoader;
+
+$loader = new UniversalClassLoader();
+$loader->registerNamespaces(array(
+    'Symfony' => __DIR__.'/vendor'
+));
