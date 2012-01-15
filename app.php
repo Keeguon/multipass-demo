fa79342206b45659363f192297b16008ec68f910
Félix Bellanger <felix.bellanger@gmail.com>
2012-01-15 16:47:17 +0100

    Updated MultiPass and oauth2-php

--- app.php
+++ app.php
@@ -7,6 +7,17 @@ require_once 'bootstrap.php';
 session_start();
 
 
+// MultiPass configuration
+// -----------------------
+
+$multipass = \MultiPass\Configuration::getInstance();
+foreach (array('facebook') as $provider) {
+  $config = \Symfony\Component\Yaml\Yaml::parse(__DIR__.'/config/'.$provider.'.yml');
+  $multipass->registerConfig($provider, $config);
+}
+$multipass->register();
+
+
 // Silex application
 // -----------------
 
@@ -16,26 +27,24 @@ $app = new \Silex\Application();
 // Routes
 // ------
 
-$app->get('/{provider}', function($provider) use($app) {
-  // Initialize MultiPass
-  $config    = \Symfony\Component\Yaml\Yaml::parse(__DIR__.'/config/'.$provider.'.yml');
-  $multipass = new \MultiPass\MultiPass($provider, $config);
-  
-  // Do the request phase routine using MultiPass
-  $multipass->request_phase();
+$app->get('/', function() use ($app) {
+  echo "<pre>";
+  print_r($_SERVER);
+  echo "</pre>";
+});
+
+$app->get('/auth/{provider}', function($provider) use ($multipass) {
+  $strategy = $multipass->getStrategy($provider);
+  $strategy->requestPhase();
 })
 ->assert('provider', '^(facebook|foursquare|github|instagram|twitter)$');
 
-$app->get('/{provider}/callback', function($provider) use($app) {
-  // Initialize MultiPass
-  $config    = \Symfony\Component\Yaml\Yaml::parse(__DIR__.'/config/'.$provider.'.yml');
-  $multipass = new \MultiPass\MultiPass($provider, $config);
-  
-  // Do the callback phase routine using MultiPass
-  $auth_hash = $multipass->callback_phase();
+$app->get('/auth/{provider}/callback', function($provider) use ($multipass) {
+  $strategy = $multipass->getStrategy($provider);
+  $authHash = $strategy->callbackPhase();
 
   echo "<pre>";
-  print_r($auth_hash->toArray());
+  print_r($authHash->toArray());
   echo "</pre>";
 })
 ->assert('provider', '^(facebook|foursquare|github|instagram|twitter)$');
